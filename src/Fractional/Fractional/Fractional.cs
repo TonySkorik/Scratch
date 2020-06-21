2efe087a9927574473ec1f3284c1eb416b0ced5d
TS <just.skorik@gmail.com>
2020-06-21 19:48:07 +0300

    Add Fractional solution for integer-based fractional arithmetic demonstration purposes

---
+++ src/Fractional/Fractional/Fractional.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Transactions;
+
+namespace Fractional
+{
+	internal class Fractional
+	{
+		private readonly long _integerPart;
+		private readonly ushort _fractionalPart;
+
+		private readonly int _fractionalPartPower;
+		private readonly bool _isNegative;
+		
+		public Fractional(string numberString)
+		{
+			var parts = numberString.Split('.', ',');
+			var integerPartString = parts[0].TrimStart('0', '-');
+			var fractionalPartString = parts.Length == 2 ? parts[1].TrimEnd('0') : "";
+			
+			_isNegative = numberString.StartsWith('-');
+
+			_integerPart = string.IsNullOrEmpty(integerPartString) ? 0 : long.Parse(integerPartString);
+			if (_integerPart != 0 && _isNegative)
+			{
+				_integerPart = -_integerPart;
+			}
+
+			_fractionalPart = string.IsNullOrEmpty(fractionalPartString) ? (ushort) 0 : ushort.Parse(fractionalPartString);
+			_fractionalPartPower = fractionalPartString.Length;
+		}
+
+		public Fractional(long integerPart, ushort fractionalPart, bool isNegative, int fractionalPartPower)
+		{
+			_integerPart = integerPart;
+			_fractionalPart = fractionalPart;
+			_isNegative = integerPart < 0 || isNegative;
+			_fractionalPartPower = fractionalPartPower;
+		}
+		
+		public Fractional Add(Fractional right)
+		{
+			int leftFractionPowerAdjustment = 0;
+			int rightFractionPowerAdjustment = 0;
+
+			if (_fractionalPartPower >= right._fractionalPartPower)
+			{
+				rightFractionPowerAdjustment = _fractionalPartPower - right._fractionalPartPower;
+			}
+			else
+			{
+				leftFractionPowerAdjustment = right._fractionalPartPower - _fractionalPartPower;
+			}
+
+			var leftInt = GetAdjustedInt(leftFractionPowerAdjustment);
+			var rightInt = right.GetAdjustedInt(rightFractionPowerAdjustment);
+			
+			var intResult = leftInt + rightInt;
+
+			// adjust comma back
+			var maxFractionPower = Math.Max(_fractionalPartPower, right._fractionalPartPower); // move comma this amount of symbols to the left
+			var ret = GetFractionalFromResult(intResult, maxFractionPower);
+			
+			return ret;
+		}
+		
+		public Fractional Subtract(Fractional right)
+		{
+			var add = right.ChangeSign();
+			return Add(add);
+		}
+
+		public Fractional Multiply(Fractional right)
+		{
+			int leftFractionPowerAdjustment = 0;
+			int rightFractionPowerAdjustment = 0;
+
+			if (_fractionalPartPower >= right._fractionalPartPower)
+			{
+				rightFractionPowerAdjustment = _fractionalPartPower - right._fractionalPartPower;
+			}
+			else
+			{
+				leftFractionPowerAdjustment = right._fractionalPartPower - _fractionalPartPower;
+			}
+
+			var leftInt = GetAdjustedInt(leftFractionPowerAdjustment);
+			var rightInt = right.GetAdjustedInt(rightFractionPowerAdjustment);
+
+			var intResult = leftInt * rightInt;
+
+			// adjust comma back
+			var maxFractionPower = Math.Max(_fractionalPartPower, right._fractionalPartPower); // move comma this amount of symbols to the left
+			var ret = GetFractionalFromResult(intResult, maxFractionPower);
+
+			return ret;
+		}
+		
+		#region Overrides
+
+		public override string ToString()
+		{
+			var fractionalPartString = _fractionalPart.ToString();
+			if (fractionalPartString.Length < _fractionalPartPower)
+			{
+				var delta = _fractionalPartPower - fractionalPartString.Length;
+				fractionalPartString = fractionalPartString.PadLeft(fractionalPartString.Length + delta, '0');
+			}
+			return $"{(_isNegative && _integerPart == 0 ? "-" : "")}{_integerPart}.{fractionalPartString}";
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Returns 0 if equal, -1 if is lower than right, 1 if greate than right
+		/// </summary>
+		/// <param name="right"></param>
+		/// <returns></returns>
+		public int CompareTo(Fractional right)
+		{
+			int leftFractionPowerAdjustment = 0;
+			int rightFractionPowerAdjustment = 0;
+
+			if (_fractionalPartPower >= right._fractionalPartPower)
+			{
+				rightFractionPowerAdjustment = _fractionalPartPower - right._fractionalPartPower;
+			}
+			else
+			{
+				leftFractionPowerAdjustment = right._fractionalPartPower - _fractionalPartPower;
+			}
+			
+			var thisInteger = GetAdjustedInt(leftFractionPowerAdjustment);
+			var rightInteger = right.GetAdjustedInt(rightFractionPowerAdjustment);
+
+			var delta = thisInteger - rightInteger;
+			
+			if (delta == 0)
+			{
+				return 0;
+			}
+
+			if (delta < 0)
+			{
+				return -1;
+			}
+
+			return 1;
+		}
+
+		#region Service methods
+
+		private Fractional GetFractionalFromResult(long result, int fractionPower)
+		{
+			var intPart = (result / (long)Math.Pow(10, fractionPower));
+			var fractionPart = (ushort)(Math.Abs(result - intPart * (long)Math.Pow(10, fractionPower)));
+
+			return new Fractional(intPart, fractionPart, result < 0, fractionPower);
+		}
+
+		private long GetAdjustedInt(int fractionPowerAdjustment)
+		{
+			var sign = _isNegative
+				? -1
+				: 1;
+
+			var ret =
+				_integerPart * (int)Math.Pow(10, _fractionalPartPower + fractionPowerAdjustment)
+				+ sign * _fractionalPart * (int)Math.Pow(10, fractionPowerAdjustment);
+
+			return ret;
+		}
+
+		private Fractional ChangeSign()
+		{
+			var newIsNegative = !_isNegative;
+			var newIntegerPart = _integerPart * -1;
+			return new Fractional(newIntegerPart, _fractionalPart, newIsNegative, _fractionalPartPower);
+		}
+
+		private int GetFractionalPower(string fractionalPartString)
+		{
+			int power = 0;
+			if (string.IsNullOrEmpty(fractionalPartString))
+			{
+				return power;
+			}
+
+			while (fractionalPartString[power] == '0')
+			{
+				power++;
+			}
+
+			return power;
+		}
+		
+		private ushort AdjustLength(ushort target, int length, int power)
+		{
+			if (target == 0)
+			{
+				return 0;
+			}
+
+			var initialLength = GetLength(target);
+			if (initialLength >= length)
+			{
+				return target;
+			}
+
+			var delta = length - initialLength;
+			ushort adjusted = (ushort)(target * (ushort)Math.Pow(10, delta - power));
+			return adjusted;
+		}
+
+		private int GetLength(uint target, int power = 0)
+		{
+			if (target < 10)
+			{
+				return 1 + power;
+			}
+
+			uint value = target;
+			int length = 1;
+
+			while (value > 0)
+			{
+				length++;
+				var divider = (uint)Math.Pow(10, length);
+				value = target / divider;
+			}
+
+			return length + power;
+		} 
+
+		#endregion
+	}
+}
---
+++ src/Fractional/Fractional/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Fractional
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Test Addition");
+			Console.WriteLine(new string('-', 10));
+			TestAdd();
+
+			Console.WriteLine("Test Subtraction");
+			Console.WriteLine(new string('-', 10));
+			TestSubtract();
+
+			Console.WriteLine("Test Product");
+			Console.WriteLine(new string('-', 10));
+			TestProduct();
+
+			Console.WriteLine("Test Comparison");
+			Console.WriteLine(new string('-', 10));
+			TestCompare();
+
+			Console.ReadKey();
+		}
+
+		private static void TestCompare()
+		{
+			Fractional f1 = new Fractional("2");
+			Fractional f2 = new Fractional("2");
+			var comparisonResult = f1.CompareTo(f2);
+			var sign = comparisonResult == 0
+				? "="
+				: comparisonResult < 0
+					? "<"
+					: ">";
+			Console.WriteLine($"{f1}{sign}{f2}");
+			
+			f1 = new Fractional("3.1");
+			f2 = new Fractional("2.53");
+			comparisonResult = f1.CompareTo(f2);
+			sign = comparisonResult == 0
+				? "="
+				: comparisonResult < 0
+					? "<"
+					: ">";
+			Console.WriteLine($"{f1}{sign}{f2}");
+		}
+
+		private static void TestProduct()
+		{
+			Fractional f1 = new Fractional("2");
+			Fractional f2 = new Fractional("2");
+			var product = f1.Multiply(f2);
+			Console.WriteLine($"{f1}*{f2}={product}");
+
+			f1 = new Fractional("2");
+			f2 = new Fractional("0");
+			product = f1.Multiply(f2);
+			Console.WriteLine($"{f1}*{f2}={product}");
+
+			f1 = new Fractional("-2");
+			f2 = new Fractional("1");
+			product = f1.Multiply(f2);
+			Console.WriteLine($"{f1}*{f2}={product}");
+			
+			f1 = new Fractional("-1567");
+			f2 = new Fractional("1567");
+			product = f1.Multiply(f2);
+			Console.WriteLine($"{f1}*{f2}={product}");
+		}
+
+		private static void TestSubtract()
+		{
+			Fractional f1 = new Fractional("1.9");
+			Fractional f2 = new Fractional("1.1");
+			var remainder = f1.Subtract(f2);
+			Console.WriteLine($"{f1}-{f2}={remainder}");
+
+			f1 = new Fractional("1.1");
+			f2 = new Fractional("1.9");
+			remainder = f1.Subtract(f2);
+			Console.WriteLine($"{f1}-{f2}={remainder}");
+		}
+
+		private static void TestAdd()
+		{
+			var f1 = new Fractional("1.0");
+			var f2 = new Fractional("-1.15");
+			var addition = f1.Add(f2);
+			Console.WriteLine($"{f1}+{f2}={addition}");
+
+			f1 = new Fractional("-0,1");
+			f2 = new Fractional("1,15");
+			addition = f1.Add(f2);
+			Console.WriteLine($"{f1}+{f2}={addition}");
+
+			f1 = new Fractional("-1.05");
+			f2 = new Fractional("1.2");
+			addition = f1.Add(f2);
+			Console.WriteLine($"{f1}+{f2}={addition}");
+
+			f1 = new Fractional("1.2");
+			f2 = new Fractional("-1.05");
+			addition = f1.Add(f2);
+			Console.WriteLine($"{f1}+{f2}={addition}");
+
+			f1 = new Fractional("1.9");
+			f2 = new Fractional("1.1");
+			addition = f1.Add(f2);
+			Console.WriteLine($"{f1}+{f2}={addition}");
+
+			f1 = new Fractional("1.38");
+			f2 = new Fractional("1.1567");
+			addition = f1.Add(f2);
+			Console.WriteLine($"{f1}+{f2}={addition}");
+
+			f1 = new Fractional("1.0");
+			f2 = new Fractional("1.15");
+			addition = f1.Add(f2);
+			Console.WriteLine($"{f1}+{f2}={addition}");
+			
+			f1 = new Fractional("0.0");
+			f2 = new Fractional("1.15");
+			addition = f1.Add(f2);
+			Console.WriteLine($"{f1}+{f2}={addition}");
+		}
+	}
+}
